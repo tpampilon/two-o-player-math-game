b2bb1cd8dd912f57f050225f1552e1c75eb3d2eb
TedP <ted.pampilon@gmail.com>
2021-04-26 19:31:07 +0000

    initial commit

---
+++ math-game-planning.rb
@@ -0,0 +1,27 @@
+class Player
+  # stores lives
+  # stores player_name
+  
+  # initializes with name and lives
+  # reader attr for player_name
+  # attr_accessor for lives
+
+  # method to substract player lives for wrong answers
+  # method to reset lives after the game
+end
+
+class Questions
+  # generate random generate random numbers
+  # method to generate questions and answers
+end
+
+class Game
+  # initializes with players 
+  # attr_accessor for all variables
+  # variable with current_player
+  
+  # game loop to run all methods (while loop) checks until player life is == 0
+  # method to call questions array
+  # method to compare the answer from gets.chomp variable with the answer
+  # method to check player lives and determine if the game should continue and call the reset to player lives
+end

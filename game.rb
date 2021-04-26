acc33767671fc2bc0d75a67f7d980702bcf1783a
TedP <ted.pampilon@gmail.com>
2021-04-26 21:39:55 +0000

    add game logic to main.rb

--- game.rb
+++ game.rb
@@ -10,14 +10,39 @@ class Game
     self.question = question
     self.current_player = self.player1
   end
+
+
   
+  def game_start
     
+    while (self.player1.lives != 0 || self.player2.lives != 0) do
+
+      puts "#{self.current_player.name}: #{self.question.generate}"
+ 
+      print "> "
+      answer = gets.chomp
+
+        if (answer.to_i == self.question.answer)
+          puts "#{self.player1.name}: #{self.player1.lives}/3 vs #{self.player2.name}: #{self.player2.lives}/3"
+        else
+          puts "#{current_player.name}: You Fool!"
+          self.current_player.deduct_life
+          puts "#{self.player1.name}: #{self.player1.lives}/3 vs #{self.player2.name}: #{self.player2.lives}/3"
+        end
+      
+        if(self.player1.lives == 0 || self.player2.lives == 0)
+          puts "------------------"
+          puts self.current_player == self.player1 ? "#{self.player2.name} wins with a score of #{self.player2.lives}/3" : "#{self.player1.name} wins with a score of #{self.player1.lives}/3"
+          puts "---- Game Over ----"
+          puts "Good bye!"
+          exit()
+        else
+          self.current_player == self.player1 ? self.current_player = self.player2 : self.current_player = self.player1
+          puts "---- New Turn ----"
+        end
+    
+    end
+  
+  end
 
 end
-
-p1 = Player.new("One")
-p2 = Player.new("Two")
-q = Questions.new
-
-game = Game.new(p1, p2, q)
-
--- main.rb
+++ main.rb
@@ -2,3 +2,10 @@ require './player'
 require './questions'
 require './game'
 
+p1 = Player.new("Player 1")
+p2 = Player.new("Player 2")
+q = Questions.new
+
+game = Game.new(p1, p2, q)
+
+game.game_start

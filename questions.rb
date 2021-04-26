0f95358f2155ca00dda71eda76769e13ab8549e1
TedP <ted.pampilon@gmail.com>
2021-04-26 20:11:58 +0000

    add question class for random question generation

--- questions.rb
+++ questions.rb
@@ -1,5 +1,21 @@
 class Questions
+  
+  attr_accessor :first, :second, :answer
+
   def initialize
-    
+    self.first = 0
+    self.second = 0
+    self.answer = self.first + self.second
   end
+
+  def generate
+    self.first = rand(10)
+    self.second = rand(10)
+    "What does #{self.first} plus #{self.second} equal?"
+  end
+
+  def answer
+    self.first + self.second
+  end
+
 end
